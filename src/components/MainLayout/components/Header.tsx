e0073809c4e1e94dff396f4d97e202f4c5ef395c
bebak16 <bebak16@gmail.com>
2022-10-06 03:42:46 +0530

    Task-2: Add and configure S3, cloudfornt.

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Mega Store!
           </Link>
         </Typography>
 
